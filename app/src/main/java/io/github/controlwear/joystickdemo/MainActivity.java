50cc94f34b0640377fd19f64c1076f1f2f53db6e
Leder, Phil <Phil.Leder@nordstrom.com>
2017-07-13 18:26:37 -0700

    Init commit of fork with mqtt added.

--- app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
+++ app/src/main/java/io/github/controlwear/joystickdemo/MainActivity.java
@@ -4,6 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 import io.github.controlwear.virtual.joystick.android.JoystickView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextViewAngleRight;
     private TextView mTextViewStrengthRight;
+
+    MqttAndroidClient mqttAndroidClient;
+
+    final String serverUri = "tcp://iot.eclipse.org:1883";
+
+    String clientId = "ExampleAndroidClient";
+    final String subscriptionTopic = "exampleAndroidTopic";
+    final String publishTopic = "exampleAndroidPublishTopic";
+    final String publishMessage = "0,0";
 
 
     @Override
@@ -43,7 +62,102 @@ public class MainActivity extends AppCompatActivity {
             public void onMove(int angle, int strength) {
                 mTextViewAngleRight.setText(angle + "°");
                 mTextViewStrengthRight.setText(strength + "%");
+                if(angle >= 90) {
+
+                }
+                else if(angle < 90) {
+
+                }
+                else if(angle > -90) {
+
+                }
             }
         });
+
+        clientId = clientId + System.currentTimeMillis();
+        mqttAndroidClient = new MqttAndroidClient(getApplicationContext(), serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+                    addToHistory("Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+                    addToHistory("Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                addToHistory("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                addToHistory("Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+    }
+    private void addToHistory(String mainText){
+        System.out.println("LOG: " + mainText);
+//        mAdapter.add(mainText);
+//        Snackbar.make(findViewById(android.R.id.content), mainText, Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show();
+
+    }
+
+    public void publishMessage(){
+
+        try {
+            MqttMessage message = new MqttMessage();
+            message.setPayload(publishMessage.getBytes());
+            mqttAndroidClient.publish(publishTopic, message);
+            addToHistory("Message Published");
+            if(!mqttAndroidClient.isConnected()){
+                addToHistory(mqttAndroidClient.getBufferedMessageCount() + " messages in buffer.");
+            }
+        } catch (MqttException e) {
+            System.err.println("Error Publishing: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    public void subscribeToTopic(){
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    addToHistory("Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    addToHistory("Failed to subscribe");
+                }
+            });
+
+            // THIS DOES NOT WORK!
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // message Arrived!
+                    System.out.println("Message: " + topic + " : " + new String(message.getPayload()));
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
     }
 }
